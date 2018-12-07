00d91e75157bcbccf85471ca34dcdb55af69966f
ChristopherStavros <christopher.kurkoski@gmail.com>
2018-12-07 00:23:52 -0600

    test1

--- Projects/Test1/main.py
+++ Projects/Test1/main.py
@@ -2,24 +2,30 @@ import json
 import csv
 import os
 
-f = open("E:/Files/Docs/GitHub/PythonTest/Data/test1.json")
+f = open("E:/Files/Docs/GitHub/PythonTest/Data/test1.json", "r+")
 lst = []
 fileData = json.load(f)
 #print("Employees")
 #print(fileData["employees"])
 
 for key in fileData:
-    print(key)
-    #lst.append(fileData[key])
     for e in fileData[key]:
-        print(e)
-        print(e["firstN ame"])
+        #print(e)
+        #print(e["ID"])
+        if(e["firstName"]=="Steez" and e["lastName"]=="McQueez"):
+            e["species"] = "strigoi"
+        else:
+            e["species"] = "vampire"
+        lst.append(e)
+         
+f.seek(0)
+json.dump(fileData, f, indent=4)
+f.truncate()
 
-    #lst.append(temp)
+f.close
 
-#print(lst)
+#print list derived from json data
+print(lst)
 
-#print(fileData)
-
-
-
+#print pure json
+print(fileData)
