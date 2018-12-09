41ebf93e1c9862246c30e11ebd7312213bed08d5
ChristopherStavros <christopher.kurkoski@gmail.com>
2018-12-08 18:23:49 -0600

    Updated main.py

--- Projects/Test1/main.py
+++ Projects/Test1/main.py
@@ -1,11 +1,11 @@
 import json, csv, os
 
+#variables
 jsonFile = open("E:/Files/Docs/GitHub/PythonTest/Data/test1.json", "r+")
 lst = []
 fileData = json.load(jsonFile)
-#print("Employees")
-#print(fileData["employees"])
 
+#main loop
 for key in fileData:
     for record in fileData[key]:
         if(record["firstName"]=="Steez" and record["lastName"]=="McQueez"):
@@ -13,15 +13,27 @@ for key in fileData:
         else:
             record["species"] = "vampire"
         lst.append(record)
-         
+
+#write to json file       
 jsonFile.seek(0)
 json.dump(fileData, jsonFile, indent=4)
 jsonFile.truncate()
 
+#cleanup
 jsonFile.close
 
+###########
+# output
+###########
+
 #print list derived from json data
+print("List derived from json data")
 print(lst)
 
+#print 'Key'
+print("Employees key")
+print(fileData["employees"])
+
 #print pure json
+print("Pure json data")
 print(fileData)
