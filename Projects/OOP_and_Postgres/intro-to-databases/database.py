c3bea490607d420097d9806541d3dd6d6c96ef24
ChristopherStavros <chriskurkoski@hotmailcom>
2019-01-26 14:00:30 -0600

    several

--- Projects/OOP_and_Postgres/intro-to-databases/database.py
+++ Projects/OOP_and_Postgres/intro-to-databases/database.py
@@ -1,7 +1,15 @@
 from psycopg2 import pool
-connection_pool = pool.SimpleConnectionPool(1, 10,  #maxconn...number of initial connections, max number of connections pool can handle
+                                           
+class ConnectionPool:
+    def __init__(self):
+        connection_pool = pool.SimpleConnectionPool(1, 1,  #maxconn...number of initial connections, max number of connections pool can handle
                                             host = "localhost", 
                                             database="learning", 
                                             user = "postgres", 
-                                            password = "P@ssw0rd") 
-                                           
+                                            password = "P@ssw0rd")
+    def __enter__(self):
+        return self.connection_pool.getconn()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # We really should be returning the connections, but how?
+        pass
