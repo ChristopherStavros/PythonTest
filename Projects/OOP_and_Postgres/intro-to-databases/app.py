7a270a93a97c8e5ea52cce309a818654c5537389
ChristopherStavros <chriskurkoski@hotmailcom>
2019-01-27 18:47:07 -0600

    multiple

--- Projects/OOP_and_Postgres/intro-to-databases/app.py
+++ Projects/OOP_and_Postgres/intro-to-databases/app.py
@@ -1,10 +1,16 @@
-from user import User 
+from database import Database
 
-my_user = User('Queezy@steezcorp', 'Queezy', 'McSteezy', None)
-my_user.save_to_db()
+Database.initialise()
 
-my_user = User('steez@steezcorp', 'Steez', 'Mcqueez', None)
-my_user.save_to_db()
 
-my_user = User.load_from_db_by_email('Queezy@steezcorp')
-print(my_user)
+
+
+#from user import User 
+# my_user = User('Queezy@steezcorp', 'Queezy', 'McSteezy', None)
+# my_user.save_to_db()
+
+# my_user = User('steez@steezcorp', 'Steez', 'Mcqueez', None)
+# my_user.save_to_db()
+
+# my_user = User.load_from_db_by_email('Queezy@steezcorp')
+# print(my_user)
--- Projects/OOP_and_Postgres/intro-to-databases/database.py
+++ Projects/OOP_and_Postgres/intro-to-databases/database.py
@@ -1,19 +1,42 @@
 from psycopg2 import pool
 
-connection_pool = pool.SimpleConnectionPool(1, 1,  #maxconn...number of initial connections, max number of connections pool can handle
+class Database:
+
+    # This value belongs to ALL objects of type Database
+    connection_pool = None
+
+    # This does NOT get executed automatically as with the __init__ method
+    @staticmethod
+    def initialise():
+        Database.connection_pool = pool.SimpleConnectionPool(1, 10,  #maxconn...number of initial connections, max number of connections pool can handle
                                     host = "localhost", 
                                     database="learning", 
                                     user = "postgres", 
                                     password = "P@ssw0rd")
+    ###  SAME
+    #  @classmethod
+    # def initialise(cls):
+    #     cls.connection_pool = pool.SimpleConnectionPool(1, 10,  #maxconn...number of initial connections, max number of connections pool can handle
+    #                                 host = "localhost", 
+    #                                 database="learning", 
+    #                                 user = "postgres", 
+    #                                 password = "P@ssw0rd")
 
-class ConnectionFromPool:
+
+class CursorFromConnectionFromPool:
     def __init__(self):
         self.connection = None
-
+        self.cursor = None
+ 
     def __enter__(self):
         self.connection = connection_pool.getconn()
-        return self.connection
+        self.cursor = self.connection.cursor()
+        return self.cursor
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.commit()
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        if exception_value is not None:
+            self.connection.rollback()
+        else:
+            self.cursor.close()
+            self.connection.commit()
         connection_pool.putconn(self.connection)
